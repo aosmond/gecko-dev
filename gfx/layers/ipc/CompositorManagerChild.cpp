61b6b55a919dc3f954a1dfa052b242ac710ef9ee
Andrew Osmond <aosmond@mozilla.com>
2017-05-24 08:02:48 -0400

    Bug 1365927 - Part 2. Add minimal PCompositorManager protocol.

---
+++ gfx/layers/ipc/CompositorManagerChild.cpp
@@ -0,0 +1,117 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/layers/CompositorManagerChild.h"
+#include "mozilla/layers/CompositorManagerParent.h"
+
+namespace mozilla {
+namespace layers {
+
+StaticRefPtr<CompositorManagerChild> CompositorManagerChild::sInstance;
+
+/* static */ bool
+CompositorManagerChild::IsInitialized()
+{
+  return sInstance && sInstance->mCanSend;
+}
+
+/* static */ bool
+CompositorManagerChild::InitSameProcess(uint32_t aNamespace)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  if (NS_WARN_IF(sInstance)) {
+    MOZ_ASSERT_UNREACHABLE("Already initialized");
+    return false;
+  }
+
+  RefPtr<CompositorManagerParent> parent =
+    CompositorManagerParent::CreateSameProcess();
+  sInstance = new CompositorManagerChild(parent, aNamespace);
+  return sInstance->mCanSend;
+}
+
+/* static */ bool
+CompositorManagerChild::Init(Endpoint<PCompositorManagerChild>&& aEndpoint,
+                             uint32_t aNamespace)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  if (sInstance) {
+    MOZ_ASSERT(sInstance->mNamespace != aNamespace);
+    if (NS_WARN_IF(sInstance->mCanSend)) {
+      MOZ_ASSERT_UNREACHABLE("Already initialized");
+      return false;
+    }
+  }
+
+  sInstance = new CompositorManagerChild(Move(aEndpoint), aNamespace);
+  return sInstance->mCanSend;
+}
+
+/* static */ void
+CompositorManagerChild::Shutdown()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  CompositorBridgeChild::ShutDown();
+
+  if (!sInstance) {
+    return;
+  }
+
+  if (sInstance->mCanSend) {
+    sInstance->Close();
+  } else if (sInstance->OtherPid() == base::GetCurrentProcId()) {
+    CompositorManagerParent::ShutdownSameProcess();
+  }
+  sInstance = nullptr;
+}
+
+CompositorManagerChild::CompositorManagerChild(CompositorManagerParent* aParent,
+                                               uint32_t aNamespace)
+  : mCanSend(false)
+  , mNamespace(aNamespace)
+  , mResourceId(0)
+{
+  MOZ_ASSERT(aParent);
+
+  SetOtherProcessId(base::GetCurrentProcId());
+  MessageLoop* loop = CompositorThreadHolder::Loop();
+  ipc::MessageChannel* channel = aParent->GetIPCChannel();
+  if (NS_WARN_IF(!Open(channel, loop, ipc::ChildSide))) {
+    return;
+  }
+
+  mCanSend = true;
+  AddRef();
+}
+
+CompositorManagerChild::CompositorManagerChild(Endpoint<PCompositorManagerChild>&& aEndpoint,
+                                               uint32_t aNamespace)
+  : mCanSend(false)
+  , mNamespace(aNamespace)
+  , mResourceId(0)
+{
+  if (NS_WARN_IF(!aEndpoint.Bind(this))) {
+    return;
+  }
+
+  mCanSend = true;
+  AddRef();
+}
+
+void
+CompositorManagerChild::DeallocPCompositorManagerChild()
+{
+  MOZ_ASSERT(!mCanSend);
+  Release();
+}
+
+void
+CompositorManagerChild::ActorDestroy(ActorDestroyReason aReason)
+{
+  mCanSend = false;
+}
+
+} // namespace layers
+} // namespace mozilla
---
+++ gfx/layers/ipc/CompositorManagerParent.cpp
@@ -0,0 +1,103 @@
+/* vim: set ts=2 sw=2 et tw=80: */
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/layers/CompositorManagerParent.h"
+#include "mozilla/layers/CompositorThread.h"
+
+namespace mozilla {
+namespace layers {
+
+StaticRefPtr<CompositorManagerParent> CompositorManagerParent::sInstance;
+StaticMutex CompositorManagerParent::sMutex;
+
+/* static */ already_AddRefed<CompositorManagerParent>
+CompositorManagerParent::CreateSameProcess()
+{
+  MOZ_ASSERT(XRE_IsParentProcess());
+  MOZ_ASSERT(NS_IsMainThread());
+  StaticMutexAutoLock lock(sMutex);
+
+  // We are creating a manager for the UI process, inside the combined GPU/UI
+  // process. It is created more-or-less the same but we retain a reference to
+  // the parent to access state.
+  if (NS_WARN_IF(sInstance)) {
+    MOZ_ASSERT_UNREACHABLE("Already initialized");
+    return nullptr;
+  }
+
+  // The child is responsible for setting up the IPC channel.
+  RefPtr<CompositorManagerParent> parent = new CompositorManagerParent();
+  parent->SetOtherProcessId(base::GetCurrentProcId());
+  sInstance = parent;
+  return parent.forget();
+}
+
+/* static */ void
+CompositorManagerParent::Create(Endpoint<PCompositorManagerParent>&& aEndpoint)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  // We are creating a manager for the another process, inside the GPU process
+  // (or UI process if it subsumbed the GPU process).
+  MOZ_ASSERT(aEndpoint.OtherPid() != base::GetCurrentProcId());
+
+  RefPtr<CompositorManagerParent> bridge = new CompositorManagerParent();
+
+  RefPtr<Runnable> runnable = NewRunnableMethod<Endpoint<PCompositorManagerParent>&&>(
+    bridge,
+    &CompositorManagerParent::Bind,
+    Move(aEndpoint));
+  CompositorThreadHolder::Loop()->PostTask(runnable.forget());
+}
+
+/* static */ void
+CompositorManagerParent::ShutdownSameProcess()
+{
+  MOZ_ASSERT(XRE_IsParentProcess());
+  MOZ_ASSERT(NS_IsMainThread());
+
+  // We rely upon the child to close the IPC channel, so this just frees our
+  // reference to the parent that we kept to access state.
+  RefPtr<CompositorManagerParent> parent;
+  {
+    StaticMutexAutoLock lock(sMutex);
+    MOZ_ASSERT(sInstance);
+    parent = sInstance.forget();
+  }
+}
+
+CompositorManagerParent::CompositorManagerParent()
+  : mCompositorThreadHolder(CompositorThreadHolder::GetSingleton())
+{ }
+
+CompositorManagerParent::~CompositorManagerParent()
+{ }
+
+void
+CompositorManagerParent::Bind(Endpoint<PCompositorManagerParent>&& aEndpoint)
+{
+  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
+  if (NS_WARN_IF(!aEndpoint.Bind(this))) {
+    return;
+  }
+
+  // Add the IPDL reference to ourself, so we can't get freed until IPDL is
+  // done with us.
+  AddRef();
+}
+
+void
+CompositorManagerParent::ActorDestroy(ActorDestroyReason aReason)
+{ }
+
+void
+CompositorManagerParent::DeallocPCompositorManagerParent()
+{
+  Release();
+}
+
+} // namespace layers
+} // namespace mozilla
