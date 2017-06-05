8fcec98d965e3957b5693b074dccd3acb2c72f59
Andrew Osmond <aosmond@mozilla.com>
2017-06-05 13:18:38 -0400

    Bug 1366097 - Part 2. Move RasterImage::mHasSize and mSize to ImageResource.

--- image/Image.cpp
+++ image/Image.cpp
@@ -199,6 +199,7 @@ ImageResource::ImageResource(ImageURL* aURI) :
   mInitialized(false),
   mAnimating(false),
   mError(false),
+  mHasSize(false),
   mImageProducerID(ImageContainer::AllocateProducerID()),
   mLastFrameID(0)
 { }
