bcb67cd977409905ae0f94bc8d7653c60402f1d7
Andrew Osmond <aosmond@mozilla.com>
2017-07-12 13:38:44 -0400

    Bug 1380341 - Fallback on the document's channel to check private browsing status if the load group has no load context.

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -3589,7 +3589,14 @@ nsContentUtils::IsInPrivateBrowsing(nsIDocument* aDoc)
 
   nsCOMPtr<nsILoadGroup> loadGroup = aDoc->GetDocumentLoadGroup();
   if (loadGroup) {
-    return IsInPrivateBrowsing(loadGroup);
+    nsCOMPtr<nsIInterfaceRequestor> callbacks;
+    loadGroup->GetNotificationCallbacks(getter_AddRefs(callbacks));
+    if (callbacks) {
+      nsCOMPtr<nsILoadContext> loadContext = do_GetInterface(callbacks);
+      if (loadContext) {
+        return loadContext->UsePrivateBrowsing();
+      }
+    }
   }
 
   nsCOMPtr<nsIChannel> channel = aDoc->GetChannel();
