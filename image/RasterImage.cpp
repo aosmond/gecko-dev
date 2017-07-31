1acccfcf3be138b823f556c8585c794747d80686
Andrew Osmond <aosmond@mozilla.com>
2017-07-31 14:28:20 -0400

    Bug 1337111 - Part 5. Add pref to force decoding of full frames, disabled by default.

--- image/RasterImage.cpp
+++ image/RasterImage.cpp
@@ -1262,6 +1262,10 @@ RasterImage::Decode(const IntSize& aSize,
   nsresult rv;
   bool animated = mAnimationState && aPlaybackType == PlaybackType::eAnimated;
   if (animated) {
+    if (gfxPrefs::ImageAnimatedGenerateFullFrames()) {
+      decoderFlags |= DecoderFlags::BLEND_ANIMATION;
+    }
+
     size_t currentFrame = mAnimationState->GetCurrentAnimationFrameIndex();
     rv = DecoderFactory::CreateAnimationDecoder(mDecoderType, WrapNotNull(this),
                                                 mSourceBuffer, mSize,
