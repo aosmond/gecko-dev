4f221c157c46b3386db11b78473f5d6cd7f48227
Andrew Osmond <aosmond@mozilla.com>
2018-05-16 15:48:54 -0400

    Bug 1462355 - Part 6. Reuse RawAccessFrameRef in FrameAnimator where possible.
    In FrameAnimator::RequestRefresh and AdvanceFrame, we currently create
    several RawAccessFrameRef objects to the same frames, either to get
    timeouts or perform the blending. With some tweaking, we can avoid
    requesting the same frame more than once. This will avoid mutex locks on
    the surface provider and the frame itself.

--- image/FrameAnimator.h
+++ image/FrameAnimator.h
@@ -348,12 +348,8 @@ private: // methods
    */
   RefreshResult AdvanceFrame(AnimationState& aState,
                              DrawableSurface& aFrames,
+                             RawAccessFrameRef& aCurrentFrame,
                              TimeStamp aTime);
-
-  /// @return the given frame's timeout if it is available
-  Maybe<FrameTimeout> GetTimeoutForFrame(AnimationState& aState,
-                                         DrawableSurface& aFrames,
-                                         uint32_t aFrameNum) const;
 
   /**
    * Get the time the frame we're currently displaying is supposed to end.
@@ -361,8 +357,8 @@ private: // methods
    * In the error case (like if the requested frame is not currently
    * decoded), returns None().
    */
-  Maybe<TimeStamp> GetCurrentImgFrameEndTime(AnimationState& aState,
-                                             DrawableSurface& aFrames) const;
+  TimeStamp GetCurrentImgFrameEndTime(AnimationState& aState,
+                                      FrameTimeout aCurrentTimeout) const;
 
   bool DoBlend(const RawAccessFrameRef& aPrevFrame,
                const RawAccessFrameRef& aNextFrame,
